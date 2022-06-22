b1a63d8499f4733bc7914f0d5f231a48a55dec8d
David Fueltala <davidfueltala90@hotmail.com>
2022-06-21 19:59:37 -0500

    taller 13

---
+++ src/main/java/com/uce/edu/demo/ProyectoU2DfApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU2DfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU2DfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/edu/demo/ProyectoU2DfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoU2DfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

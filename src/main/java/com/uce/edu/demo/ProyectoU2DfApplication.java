5e8ede4d16f9ea2d5e33f680a8a1f4a1240a9f14
David Fueltala <davidfueltala90@hotmail.com>
2022-06-27 17:01:02 -0500

    Tarea 12

--- src/main/java/com/uce/edu/demo/ProyectoU2DfApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2DfApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.log4j.Logger;
 @SpringBootApplication
 public class ProyectoU2DfApplication implements CommandLineRunner{
+	
+	private static Logger log = Logger.getLogger(ProyectoU2DfApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2DfApplication.class, args);
@@ -15,7 +18,9 @@ public class ProyectoU2DfApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hola Mundo");
+//		System.out.println("Hola Mundo");
+		
+		log.info("Hola mundo desde LOG");
 		
 	}
 
